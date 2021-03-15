47351609c013ca7b32666e954559c5dfeb010fd0
Sarthak Sharma <19bcs1330@cuchd.in>
2021-03-15 16:57:10 +0530

    Basic Flask + Folders

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template("test.html", password=9)
 
 
+print(__name__)
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
