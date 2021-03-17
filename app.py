24c405c864d0e43173755da18c49428049e89fe0
Sarthak Sharma <19bcs1330@cuchd.in>
2021-03-17 23:07:36 +0530

    addred html, drawing needs improvement

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return render_template("test.html", password=9)
+@app.route('/', methods=["POST", "GET"])
+def main_page():
+    return render_template("MainPage.html")
+
+
+@app.route('/3')
+def result_page(input_value):
+    return render_template("ResultPage.html", result=3)
 
 
 print(__name__)
 if __name__ == '__main__':
     app.run(debug=True)
-
