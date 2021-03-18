58953c71262e1dec0d3fa98df81ecac71a3fcc1e
Sarthak Sharma <19bcs1330@cuchd.in>
2021-03-18 23:09:12 +0530

    Image can be retrieved now!

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+import cv2
+import numpy as np
+import base64
 app = Flask(__name__)
 
 
@@ -8,8 +10,18 @@ def main_page():
     return render_template("MainPage.html")
 
 
-@app.route('/3')
-def result_page(input_value):
+@app.route('/3', methods = ['POST'])
+def result_page():
+    draw = request.form.get("url")
+    draw = draw[21:]
+    # Decoding
+    draw_decoded = base64.b64decode(draw)
+    image = np.asarray(bytearray(draw_decoded), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_GRAYSCALE)
+    # Show Image, uncomment to see image
+    # cv2.imshow('img', image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
     return render_template("ResultPage.html", result=3)
 
 
